8e8a06b5e70ad9e18caf0b19a6179a075c73c2fb
navins94 <nikshingote@gmail.com>
2025-01-20 19:44:56 +0530

    change mail settings

--- backend/config/env/development/plugins.js
+++ backend/config/env/development/plugins.js
@@ -3,28 +3,16 @@ module.exports = ({ env }) => ({
     config: {
       provider: 'nodemailer',
       providerOptions: {
-        host: env('SMTP_HOST'),
-        port: env('SMTP_PORT'),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
-        },
-        secure: false, // Changed to false for port 587
-        requireTLS: true, // Added this
-        tls: {
-          ciphers: 'SSLv3', // Added this
-          rejectUnauthorized: false, // Added this
-        },
-      },
-      settings: {
-        defaultFrom: env('SMTP_FROM'),
-        defaultReplyTo: env('SMTP_FROM'),
+        host: 'mailpit',
+        port: 1025,
+        ignoreTLS: true,
+        auth: false,
       },
     },
-  },
-  'users-permissions': {
-    config: {
-      email_confirmation: false,
+    'users-permissions': {
+      config: {
+        email_confirmation: false,
+      },
     },
   },
 });
--- backend/config/plugins.js
+++ backend/config/plugins.js
@@ -1,29 +1,17 @@
 module.exports = ({ env }) => ({
-  'users-permissions': {
-    config: {
-      email_confirmation: false,
-    },
-  },
   email: {
     config: {
       provider: 'nodemailer',
       providerOptions: {
-        host: env('SMTP_HOST'),
-        port: env('SMTP_PORT'),
-        auth: {
-          user: env('SMTP_USERNAME'),
-          pass: env('SMTP_PASSWORD'),
-        },
-        secure: false, // Changed to false for port 587
-        requireTLS: true, // Added this
-        tls: {
-          ciphers: 'SSLv3', // Added this
-          rejectUnauthorized: false, // Added this
-        },
+        host: 'mailpit',
+        port: 1025,
+        ignoreTLS: true,
+        auth: false,
       },
-      settings: {
-        defaultFrom: env('SMTP_FROM'),
-        defaultReplyTo: env('SMTP_FROM'),
+    },
+    'users-permissions': {
+      config: {
+        email_confirmation: false,
       },
     },
   },
