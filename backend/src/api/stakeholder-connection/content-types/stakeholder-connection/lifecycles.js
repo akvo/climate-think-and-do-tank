86281ec15eefb7ee24805ae909d216fb708e0431
zuhdil <zuhdil@gmail.com>
2025-03-18 15:37:45 +0700

    Send email when a Stakeholder Connection is requested

--- backend/src/api/stakeholder-connection/content-types/stakeholder-connection/lifecycles.js
+++ backend/src/api/stakeholder-connection/content-types/stakeholder-connection/lifecycles.js
@@ -1,4 +1,35 @@
 module.exports = {
+  async afterCreate(event) {
+    const { documentId } = event.result;
+    try {
+      const entity = await strapi
+        .documents('api::stakeholder-connection.stakeholder-connection')
+        .findOne({
+          documentId,
+          populate: {
+            requester: {
+              fields: ['email', 'full_name'],
+            },
+            receiver: {
+              fields: ['email', 'full_name'],
+            }
+          },
+        });
+
+      const receiver = entity.receiver.full_name || entity.receiver.email;
+      const requester = entity.requester.full_name || entity.requester.email;
+      const message = `<p>Hi ${receiver},</p>\n\n<p>You got a connection request from ${requester}.</p>\n\n<p>Cheers!</p>`
+
+      await strapi.plugin('email').service('email').send({
+        to: entity.receiver.email,
+        subject: 'You Got a Connection Request',
+        text: message,
+        html: message,
+      });
+    } catch (err) {
+      strapi.log.error(err);
+    }
+  },
 
   beforeUpdate(event) {
     const { data } = event.params;
