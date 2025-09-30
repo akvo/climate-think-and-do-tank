eb4efca20d270e01d0d4fdafdc0510aa3d9a12bc
zuhdil <zuhdil@gmail.com>
2025-09-30 11:03:45 +0700

    fix: move Piwik analytics from _document to _app

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -1,10 +1,7 @@
 import { Html, Head, Main, NextScript } from 'next/document';
 import Script from 'next/script';
-import { env } from '../helpers/env-vars.js';
 
 export default function Document() {
-  const piwikSiteId = env('NEXT_PUBLIC_PIWIK_SITE_ID');
-
   return (
     <Html lang="en">
       <Head>
@@ -24,18 +21,6 @@ export default function Document() {
       <body className="antialiased">
         <Main />
         <Script strategy="beforeInteractive" src="/__ENV.js" />
-        {piwikSiteId && (
-          <Script strategy="beforeInteractive" id="piwik-analytics">
-            {`
-(function(window, document, dataLayerName, id) {
-window[dataLayerName]=window[dataLayerName]||[],window[dataLayerName].push({start:(new Date).getTime(),event:"stg.start"});var scripts=document.getElementsByTagName('script')[0],tags=document.createElement('script');
-var qP=[];dataLayerName!=="dataLayer"&&qP.push("data_layer_name="+dataLayerName);var qPString=qP.length>0?("?"+qP.join("&")):"";
-tags.async=!0,tags.src="https://akvo.containers.piwik.pro/"+id+".js"+qPString,scripts.parentNode.insertBefore(tags,scripts);
-!function(a,n,i){a[n]=a[n]||{};for(var c=0;c<i.length;c++)!function(i){a[n][i]=a[n][i]||{},a[n][i].api=a[n][i].api||function(){var a=[].slice.call(arguments,0);"string"==typeof a[0]&&window[dataLayerName].push({event:n+"."+i+":"+a[0],parameters:[].slice.call(arguments,1)})}}(i[c])}(window,"ppms",["tm","cm"]);
-})(window, document, 'dataLayer', '${piwikSiteId}');
-            `}
-          </Script>
-        )}
         <NextScript />
       </body>
     </Html>
